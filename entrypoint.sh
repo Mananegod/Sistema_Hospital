cb82c8c0b767657450b08e68f9edd3e7d10e6f3b
VinkeLevinke <119265602+VinkeLevinke@users.noreply.github.com>
2026-07-09 00:45:53 -0400

    Cambios en el Bash 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,21 +7,7 @@ fi
 
 php artisan migrate --force
 
-SEED_NEEDED=$(php -r "
-try {
-    \DB::connection()->getPdo();
-    if (\Schema::hasTable('sectores') && \DB::table('sectores')->count() > 0) {
-        echo '0';
-    } else {
-        echo '1';
-    }
-} catch (Exception \$e) {
-    echo '1';
-}
-" 2>/dev/null)
 
-if [ "$SEED_NEEDED" == "1" ]; then
-    php artisan db:seed --force
-fi
+php artisan db:seed --force
 
 exec apache2-foreground
