f1c5bf7d6a65652cf7e264f8b43204af29ac33ef
VinkeLevinke <119265602+VinkeLevinke@users.noreply.github.com>
2026-07-09 15:42:10 -0400

    prueba

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\RateLimiter; 
-use Illuminate\Support\Str; 
-use App\Models\User;
+use App\Models\User; 
 
 class AuthController extends Controller
 {
@@ -23,30 +21,13 @@ class AuthController extends Controller
             'password' => 'required|string',
         ]);
 
-       
-        $throttleKey = Str::lower($credentials['nombre']) . '|' . $request->ip();
-
-        
-        if (RateLimiter::tooManyAttempts($throttleKey, 5)) {//los intentos hasta que falle
-            $seconds = RateLimiter::availableIn($throttleKey);
-            return back()->withErrors(['error' => "Demasiados intentos de inicio de sesión. Por seguridad, intente de nuevo en {$seconds} segundos."]);
-        }
-
         
         $user = User::whereRaw('LOWER(nombre) = ?', [strtolower($credentials['nombre'])])->first();
 
-       
         if ($user && Auth::attempt(['nombre' => $user->nombre, 'password' => $credentials['password']])) {
-            
-           
-            RateLimiter::clear($throttleKey);
-            
             $request->session()->regenerate();
             return redirect()->intended(route('home'));
         }
-
-       
-        RateLimiter::hit($throttleKey, 10); // SEGUNDOS DE REINTENTO
 
         return back()->withErrors(['error' => 'Las credenciales introducidas son incorrectas.']);
     }
