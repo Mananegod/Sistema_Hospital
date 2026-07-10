20a16138581f39a9510bdd0efb0d140cdd6f7b2a
VinkeLevinke <119265602+VinkeLevinke@users.noreply.github.com>
2026-07-10 03:12:19 -0400

    dios

--- config/database.php
+++ config/database.php
@@ -96,7 +96,13 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'sslmode' => 'require',                       
+            'connect_timeout' => 60,                      
+            'options' => [
+                PDO::ATTR_EMULATE_PREPARES => true,
+                PDO::ATTR_PERSISTENT => false,
+                PDO::ATTR_TIMEOUT => 60,                  
+            ],
         ],
 
         'sqlsrv' => [
@@ -149,7 +155,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
