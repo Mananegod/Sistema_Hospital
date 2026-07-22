f0edcda08bf930fc00dec99167dac5a9e9d383d0
VinkeLevinke <119265602+VinkeLevinke@users.noreply.github.com>
2026-07-22 19:48:14 -0400

    oh shit, here we go again

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,19 @@
-
+#!/bin/bash
 set -e
+
 
 if [ ! -f "artisan" ]; then
     exec apache2-foreground
 fi
 
 
+php artisan config:clear
+
+
 echo "Running migrations..."
-php artisan migrate:fresh
+php artisan migrate --force --no-interaction
 
 echo "Running seeders..."
-php artisan db:seed --force
+php artisan db:seed --force --no-interaction
 
 exec apache2-foreground
