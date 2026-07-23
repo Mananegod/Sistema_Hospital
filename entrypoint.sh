20b4fe1a329f6c232aa89c87e4781f04ac574b3c
VinkeLevinke <119265602+VinkeLevinke@users.noreply.github.com>
2026-07-23 11:49:58 -0400

    saliendo de reintentos

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,16 +5,16 @@ if [ ! -f "artisan" ]; then
     exec apache2-foreground
 fi
 
-wait_for_db() {
-    echo "Esperando conexión a la base de datos..."
-    until php artisan db:show > /dev/null 2>&1; do
-        echo "Base de datos no disponible, reintentando en 2 segundos..."
-        sleep 2
-    done
-    echo "Base de datos conectada correctamente."
-}
+# wait_for_db() {
+#     echo "Esperando conexión a la base de datos..."
+#     until php artisan db:show > /dev/null 2>&1; do
+#         echo "Base de datos no disponible, reintentando en 2 segundos..."
+#         sleep 2
+#     done
+#     echo "Base de datos conectada correctamente."
+# }
 
-wait_for_db
+# wait_for_db
 
 
 php artisan config:clear
